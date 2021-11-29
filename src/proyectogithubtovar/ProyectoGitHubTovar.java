3f62e68d6b9da82ed47c20acb07edd837dd5c0c8
ttovarra <lenovo@DESKTOP-SBACIGA>
2021-11-29 11:36:14 -0600

    Prueba 1

---
+++ src/proyectogithubtovar/ProyectoGitHubTovar.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectogithubtovar;
+
+/**
+ *
+ * @author lenovo
+ */
+public class ProyectoGitHubTovar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          for (int i = 0; i < 10000; i++) {
+              System.out.println("Prueba Git Hub Espfuerzo 1: "+i);
+        }
+    }
+    
+}
